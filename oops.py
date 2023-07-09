13c1657c25e3fa02e18b83e964c4ad034dcc8bf8
curiousthinker85 <adityaasabe852002@gmail.com>
2023-07-09 18:03:35 +0530

    super

--- oops.py
+++ oops.py
@@ -90,18 +90,81 @@
 
 # Class Method
 
-class Employee:
-    company = "Apple"
-    def show(self):
-        print(f"The name is {self.name} and company is {self.company}")
-    @classmethod
-    def change_company(cls, new_company):
-        cls.company = new_company
+# class Employee:
+#     company = "Apple"
+#     def show(self):
+#         print(f"The name is {self.name} and company is {self.company}")
+#     @classmethod
+#     def change_company(cls, new_company):
+#         cls.company = new_company
 
 
-e1 = Employee()
-e1.name = "Aditya"
-print(e1.show())
-e1.change_company("Google")
-print(e1.show())
-print(Employee.company)
+# e1 = Employee()
+# e1.name = "Aditya"
+# print(e1.show())
+# e1.change_company("Google")
+# print(e1.show())
+# print(Employee.company)
+
+
+# class Employee:
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+
+#     @classmethod
+#     def fromstr(cls, string):
+#         return cls(string.split("-")[0],string.split("-")[1])
+
+
+
+# e1 = Employee("Aditya", 120000)
+# print(e1.name,e1.salary)
+
+# string = "Abhi-120000"
+# e2 = Employee.fromstr(string)
+# print(e2.name, e2.salary)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     @classmethod
+#     def from_string(cls, string):
+#         name, age = string.split(',')
+#         return cls(name, int(age))
+    
+# person = Person.from_string("John Doe, 30")
+# print(person.name, person.age)
+
+# class Rectangle:
+#   def __init__(self, width, height):
+#     self.width = width
+#     self.height = height
+
+#   @classmethod
+#   def square(cls, size):
+#     return cls(size, size)
+  
+# rectangle = Rectangle.square(10)
+
+# print(rectangle.width, rectangle.height)
+
+## Super keyword
+
+# class Employee:
+#     def __init__(self, name, id):
+#         self.name = name
+#         self.id = id
+
+# class Programmer(Employee):
+#     def __init__(self, name, id, lang):
+#         super().__init__(name, id)
+#         self.lang = lang
+
+# e1 = Employee("John", 124)
+# print(e1.name, e1.id)
+
+# e2 = Programmer("Aditya", 342, "Python")
+# print(e2.name,e2.id,e2.lang)
